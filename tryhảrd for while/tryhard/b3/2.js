d8f28886ca8c058ce7c81d765f73b946cef4a8b6
reishane <hoangnam69689>
2019-02-27 17:01:59 +0700

    show fibo

--- tryhảrd for while/tryhard/b3/2.js
+++ tryhảrd for while/tryhard/b3/2.js
@@ -1,6 +1,14 @@
-var i = 1;
-var j = 0;
-var cc = i + j;
-for(i = 1 + 1; i <=144; i ++) {
-    document.write(i);
+
+var i = 0;
+var j = 1;
+var f;
+var num = 20;
+document.write(i + "<br/>");
+document.write(j +"<br/>");
+for(var o = 3; i < num; i++){
+    f = i + j;
+    i = j;
+    j = f;
+    document.write(f + "<br/>");
 }
+////////////////////////////////////////////////////////////////////
