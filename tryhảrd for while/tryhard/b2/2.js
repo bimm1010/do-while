b73a6911179f9bf79378d57afbaeaedd37c15bf3
reishane <hoangnam69689>
2019-02-27 15:48:23 +0700

    tryhary star

---
+++ tryhảrd for while/tryhard/b2/2.js
@@ -0,0 +1,10 @@
+var i = prompt("nhập nhiệt độ hiện tại");
+for (; ;){
+    if(i <= 100){
+
+    }if (i > 100){
+        alert("giảm nhiệt độ");
+    }else if ( i < 20){
+        alert("tăng nhiệt độ");
+    }
+}
---
+++ tryhảrd for while/tryhard/b3/2.js
@@ -0,0 +1,6 @@
+var i = 1;
+var j = 0;
+var cc = i + j;
+for(i = 1 + 1; i <=144; i ++) {
+    document.write(i);
+}
